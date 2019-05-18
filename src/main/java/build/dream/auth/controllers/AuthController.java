3fac656dfb300384fa40bad9ff079aa2372f8269
liuyandong <554030404@qq.com>
2019-05-19 03:19:40 +0800

    初始化项目！！

--- src/main/java/build/dream/auth/controllers/AuthController.java
+++ src/main/java/build/dream/auth/controllers/AuthController.java
@@ -1,29 +1,13 @@
 package build.dream.auth.controllers;
 
-import build.dream.common.utils.ApplicationHandler;
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
-import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping(value = "/auth")
 public class AuthController {
-    private HttpSessionRequestCache httpSessionRequestCache = new HttpSessionRequestCache();
-
     @RequestMapping(value = "/index")
     public String index() {
-        HttpServletRequest httpServletRequest = ApplicationHandler.getHttpServletRequest();
-        HttpServletResponse httpServletResponse = ApplicationHandler.getHttpServletResponse();
-        SavedRequest savedRequest = httpSessionRequestCache.getRequest(httpServletRequest, httpServletResponse);
-        if (savedRequest != null) {
-            httpSessionRequestCache.removeRequest(httpServletRequest, httpServletResponse);
-            return "auth/index";
-        } else {
-            return "auth/error";
-        }
+        return "auth/index";
     }
 }
--- src/main/webapp/WEB-INF/views/auth/error.jsp
+++ src/main/webapp/WEB-INF/views/auth/error.jsp
@@ -8,9 +8,9 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Title</title>
+    <title>错误页面</title>
 </head>
 <body>
-错误
+出错了！
 </body>
 </html>
