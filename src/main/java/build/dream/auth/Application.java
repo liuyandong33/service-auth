27a03d125171a32c4505d72de70128944b4c25a2
liuyandong <554030404@qq.com>
2019-05-20 13:22:57 +0800

    错误修改！！

--- src/main/java/build/dream/auth/Application.java
+++ src/main/java/build/dream/auth/Application.java
@@ -4,9 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @ServletComponentScan
+@EnableDiscoveryClient
+@EnableRedisHttpSession
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
