9f4957ba484feb2b0840af6025ddf42d314ab67b
liuyandong <554030404@qq.com>
2020-03-04 02:03:16 +0800

    初始化项目！！

--- src/main/java/build/dream/auth/Application.java
+++ src/main/java/build/dream/auth/Application.java
@@ -3,12 +3,11 @@ package build.dream.auth;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 @ServletComponentScan
 @EnableDiscoveryClient
 @EnableRedisHttpSession
